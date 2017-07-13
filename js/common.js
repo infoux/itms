d9f437698882f5a01832d9ade859787f1d2c5b7e
rhkanrdl <emrdl7@gmail.com>
2017-07-13 18:09:51 +0900

    완료

--- js/common.js
+++ js/common.js
@@ -1,3 +1,39 @@
+function addCellHeader(table) {
+  if (!table) {
+    return false;
+  }
+
+  var trs = table.getElementsByTagName('tr');
+  var trsChild;
+  var grid = {};
+  var cells;
+  for (var i = 0, cntI = trs.length; i < cntI; i++) {
+    if (!grid[i]) {
+      grid[i] = {};
+    }
+    trsChild = trs.item(i).childNodes;
+    cells = 0;
+    for (var j = 0, cntJ = trsChild.length; j < cntJ; j++) {
+      if (trsChild[j].nodeType == 1) {
+        grid[i][cells++] = trsChild[j];
+      }
+    }
+  }
+  var cellHeader = '';
+  for (row in grid) {
+    if (row == 0) {
+      continue;
+    }
+    for (cell in grid[row]) {
+      cellHeader = grid[0][cell].innerHTML;
+      grid[row][cell].setAttribute('data-cell-header', cellHeader);
+    }
+  }
+}
+
+
+
+
 $(document).ready(function() {
   /*
 
@@ -14,6 +50,8 @@ $(document).ready(function() {
     }
   }
   viewMode();
+
+  addCellHeader(document.querySelector('.respond-table'));
   $(window).resize(function() {
     viewMode();
   });
